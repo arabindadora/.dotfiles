9f2fe23f48786be135416c22b71bb14e608ee214
Arabinda Dora <arabinda.dora@klarna.com>
2022-11-05 18:08:21 +0100

    add keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,30 +1,48 @@
 local g = vim.g -- a table to access global variables
 local map = vim.keymap
 
+local opts = { silent = true }
+
 -- make space the leader key
-map.set('', '<Space>', '<Nop>', { silent = true })
+map.set('', '<Space>', '<Nop>', opts)
 g.mapleader = ' '
 g.maplocalleader = ' '
 
-map.set('', '<leader>c', '"+y') -- Copy to clipboard in normal, visual, select and operator modes
-map.set('i', '<C-u>', '<C-g>u<C-u>') -- Make <C-u> undo-friendly
-map.set('i', '<C-w>', '<C-g>u<C-w>') -- Make <C-w> undo-friendly
+-- source this file
+map.set("n", "<leader>ss", "<cmd>so %<CR>", opts)
 
 -- <Tab> to navigate the completion menu
 map.set('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<Tab>"', { expr = true })
 map.set('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', { expr = true })
 
 map.set('n', '<C-l>', '<cmd>noh<CR>') -- Clear highlights
-map.set('n', '<leader>o', 'm`o<Esc>``') -- Insert a newline in normal mode
 
 map.set('n', ';', ':')
 map.set('n', '<leader>w', '<cmd>:w<CR>')
 
 map.set('n', '<leader>gg',
-  ':FloatermNew --height=0.9 --width=0.9 --wintype=float --title=lazygit lazygit<CR>', { silent = true })
+  ':FloatermNew --height=0.9 --width=0.9 --wintype=float --title=lazygit lazygit<CR>', opts)
 
 -- remap increment/decrement shortcuts
 map.set('n', '+', '<C-a>')
 map.set('n', '-', '<C-x>')
-map.set('', '<C-a>', '<Nop>', { silent = true })
-map.set('', '<C-x>', '<Nop>', { silent = true })
+
+map.set('', '<C-a>', '<Esc>ggVG<CR>', opts) -- select all
+map.set('', '<C-c>', '"+y') -- copy to clipboard
+
+-- manual indentation - stay in visual mode
+map.set("v", "<", "<gv", opts)
+map.set("v", ">", ">gv", opts)
+
+-- tabs and buffers
+map.set('', '<C-n>', '<cmd>:tabnew<cr>')
+map.set('', '<C-x>', '<cmd>:tabclose<CR>', opts)
+map.set("n", "]t", "<cmd>:tabnext<CR>", opts)
+map.set("n", "[t", "<cmd>:tabprevious<CR>", opts)
+map.set("n", "]b", "<cmd>:bnext<CR>", opts)
+map.set("n", "[b", "<cmd>:bprevious<CR>", opts)
+
+-- move line up/down
+map.set("n", "<S-u>", "<Esc>:m .-2<CR>", opts)
+map.set("n", "<S-d>", "<Esc>:m .+1<CR>", opts)
+
