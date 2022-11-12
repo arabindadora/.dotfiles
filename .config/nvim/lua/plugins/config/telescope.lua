98a62b5592db699779b3357654760f7cd068ce74
Arabinda Dora <arabinda.dora@klarna.com>
2022-11-12 12:59:39 +0100

    refactor telescope keymaps

--- .config/nvim/lua/plugins/config/telescope.lua
+++ .config/nvim/lua/plugins/config/telescope.lua
@@ -1,4 +1,6 @@
 local telescope = require('telescope')
+local map = vim.keymap
+local opts = { silent = true }
 
 telescope.setup {
   defaults = {
@@ -12,12 +14,10 @@ telescope.setup {
 }
 
 -- Add leader shortcuts
-vim.keymap.set('n', '<leader>bb', [[<cmd>lua require('telescope.builtin').buffers()<CR>]], { silent = true })
-vim.keymap.set('n', '<leader><space>', [[<cmd>lua require('telescope.builtin').find_files({previewer = false})<CR>]],
-  { silent = true })
-vim.keymap.set('n', '<leader>fb', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]],
-  { silent = true })
-vim.keymap.set('n', '<leader>gs', [[<cmd>lua require('telescope.builtin').grep_string()<CR>]], { silent = true })
-vim.keymap.set('n', '<leader>gl', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], { silent = true })
-vim.keymap.set('n', '<leader>?', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]], { silent = true })
-vim.keymap.set('n', '<leader>f', [[<cmd>lua vim.lsp.buf.format({async = true})<CR>]], { silent = true })
+map.set('n', '<leader>bb', [[<cmd>lua require('telescope.builtin').buffers()<CR>]], opts)
+map.set('n', '<leader><space>', [[<cmd>lua require('telescope.builtin').find_files({previewer = false})<CR>]], opts)
+map.set('n', '<leader>fb', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]], opts)
+map.set('n', '<leader>gs', [[<cmd>lua require('telescope.builtin').grep_string()<CR>]], opts)
+map.set('n', '<leader>gl', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], opts)
+map.set('n', '<leader>?', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]], opts)
+map.set('n', '<leader>f', [[<cmd>lua vim.lsp.buf.format({async = true})<CR>]], opts)
