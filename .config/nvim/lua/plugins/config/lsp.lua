8e8acb321accd918d9aa467c2ae14c4f2694830a
Arabinda Dora <arabinda.dora@klarna.com>
2022-12-16 18:12:26 +0100

    add terraform intellisense

--- .config/nvim/lua/plugins/config/lsp.lua
+++ .config/nvim/lua/plugins/config/lsp.lua
@@ -71,3 +71,13 @@ lspconfig['sumneko_lua'].setup({
     },
   },
 })
+
+-- https://mukeshsharma.dev/2022/02/08/neovim-workflow-for-terraform.html
+vim.cmd([[silent! autocmd! filetypedetect BufRead,BufNewFile *.tf]])
+vim.cmd([[autocmd BufRead,BufNewFile *.hcl set filetype=hcl]])
+vim.cmd([[autocmd BufRead,BufNewFile .terraformrc,terraform.rc set filetype=hcl]])
+vim.cmd([[autocmd BufRead,BufNewFile *.tf,*.tfvars set filetype=terraform]])
+vim.cmd([[autocmd BufRead,BufNewFile *.tfstate,*.tfstate.backup set filetype=json]])
+vim.cmd([[let g:terraform_fmt_on_save=1]])
+vim.cmd([[let g:terraform_align=1]])
+
