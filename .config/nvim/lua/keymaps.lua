dbeabce49c819c03bcbc5aeb2749fbe0e12412c9
Arabinda Dora <arabinda.dora@klarna.com>
2022-12-16 18:11:42 +0100

    add new keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,48 +1,64 @@
 local g = vim.g -- a table to access global variables
-local map = vim.keymap
+local map = vim.keymap.set
 
 local opts = { silent = true }
 
 -- make space the leader key
-map.set('', '<Space>', '<Nop>', opts)
+map('', '<Space>', '<Nop>', opts)
 g.mapleader = ' '
 g.maplocalleader = ' '
 
 -- source this file
-map.set("n", "<leader>ss", "<cmd>so %<CR>", opts)
+map("n", "<leader>ss", "<cmd>so %<CR>", opts)
+map("n", "<leader>rg", "<cmd>!go run %<CR>", opts)
 
 -- <Tab> to navigate the completion menu
-map.set('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<Tab>"', { expr = true })
-map.set('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', { expr = true })
+map('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<Tab>"', { expr = true })
+map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', { expr = true })
 
-map.set('n', '<C-l>', '<cmd>noh<CR>') -- Clear highlights
+map('n', '<C-l>', '<cmd>noh<CR>') -- Clear highlights
 
-map.set('n', ';', ':')
-map.set('n', '<leader>w', '<cmd>:w<CR>')
+map('n', ';', ':')
+map('v', ';', ':')
+map('n', '<leader>w', '<cmd>:w<CR>')
 
-map.set('n', '<leader>gg',
+map('n', '<leader>gg',
   ':FloatermNew --height=0.9 --width=0.9 --wintype=float --title=lazygit lazygit<CR>', opts)
 
 -- remap increment/decrement shortcuts
-map.set('n', '+', '<C-a>')
-map.set('n', '-', '<C-x>')
+map('n', '+', '<C-a>')
+map('n', '-', '<C-x>')
 
-map.set('', '<C-a>', '<Esc>ggVG<CR>', opts) -- select all
-map.set('', '<C-c>', '"+y') -- copy to clipboard
+map('', '<C-a>', '<Esc>ggVG<CR>', opts) -- select all
+map('', '<C-c>', '"+y') -- copy to clipboard
 
 -- manual indentation - stay in visual mode
-map.set("v", "<", "<gv", opts)
-map.set("v", ">", ">gv", opts)
+map("v", "<", "<gv", opts)
+map("v", ">", ">gv", opts)
 
 -- tabs and buffers
-map.set('', '<C-n>', '<cmd>:tabnew<cr>')
-map.set('', '<C-x>', '<cmd>:tabclose<CR>', opts)
-map.set("n", "]t", "<cmd>:tabnext<CR>", opts)
-map.set("n", "[t", "<cmd>:tabprevious<CR>", opts)
-map.set("n", "]b", "<cmd>:bnext<CR>", opts)
-map.set("n", "[b", "<cmd>:bprevious<CR>", opts)
+map('', '<C-n>', '<cmd>:tabnew<cr>')
+map('', '<C-w>', '<cmd>:tabclose<CR>', opts)
+map("n", "]t", "<cmd>:tabnext<CR>", opts)
+map("n", "[t", "<cmd>:tabprevious<CR>", opts)
+map("n", "]b", "<cmd>:bnext<CR>", opts)
+map("n", "[b", "<cmd>:bprevious<CR>", opts)
 
 -- move line up/down
-map.set("n", "<S-u>", "<Esc>:m .-2<CR>", opts)
-map.set("n", "<S-d>", "<Esc>:m .+1<CR>", opts)
+map("n", "<S-u>", "<Esc>:m .-2<CR>", opts)
+map("n", "<S-d>", "<Esc>:m .+1<CR>", opts)
+
+-- Don't copy when pasting over selection
+map("v", "p", '"_dP')
+map("v", "P", '"_dp')
+
+-- go to beginning and end
+map("i", "<C-b>", "<ESC>^i") -- " beginning of line"
+map("i", "<C-e>", "<End>") -- " end of line"
+
+-- navigate within insert mode
+map("i", "<C-h>", "<Left>") -- "  move left"
+map("i", "<C-l>", "<Right>") -- " move right"
+map("i", "<C-j>", "<Down>") -- " move down"
+map("i", "<C-k>", "<Up>") -- " move up"
 
